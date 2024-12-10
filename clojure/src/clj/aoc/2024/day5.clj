6d9652abaf6158ace033720e20fb2e9fbf165e59
Sunil Gunisetty <sunilgunisetty@gmail.com>
2024-12-10 14:54:19 -0500

    2024: Clojure - day5

---
+++ clojure/src/clj/aoc/2024/day5.clj
@@ -0,0 +1,70 @@
+(ns aoc.2024.day5
+  (:require
+   [clojure.string :as string]
+   [clojure.java.io :as io]))
+
+(defn parse-file
+  [file]
+  (as-> file $
+    (slurp $)
+    (string/split $ #"\n\n")
+    (map string/split-lines $)))
+
+(def sample-input (parse-file "../input/2024/day5-sample.txt"))
+(def input (parse-file "../input/2024/day5.txt"))
+
+(defn process-ordering
+  [ordering]
+  (reduce
+   (fn [acc v]
+     (let [[k v] (string/split v #"\|")]
+       (update acc k #((fnil conj #{}) % v))))
+   {} ordering))
+
+(defn valid-update
+  [orderings update]
+  (loop [[x & xs] update
+         res      false]
+    (if (seq xs)
+      (if (empty? (clojure.set/difference (into #{} xs) (get orderings x)))
+        (recur xs true)
+        false)
+      res)))
+
+(defn get-mid-val
+  [update]
+  (get update (int (/ (count update) 2))))
+
+(defn day5-part1
+  [input]
+  (let [orderings (process-ordering (first input))]
+    (->> input
+         second
+         (map #(string/split % #","))
+         (filter #(valid-update orderings %))
+         (map get-mid-val)
+         (map #(Integer/parseInt %))
+         (apply +))))
+
+(defn make-comparator
+  [orderings]
+  (let [parsed-ordersings
+        (reduce
+         (fn [acc v]
+           (conj acc (string/split v #"\|")))
+         #{} orderings)]
+    (fn [a b]
+      (if (contains? parsed-ordersings [a b]) -1 1))))
+
+(defn day5-part2
+  [input]
+  (let [comparator (make-comparator (first input))
+        orderings  (process-ordering (first input))]
+    (->> input
+         second
+         (map #(string/split % #","))
+         (remove #(valid-update orderings %))
+         (map #(sort comparator %))
+         (map (fn [v] (get-mid-val (vec v))))
+         (map #(Integer/parseInt %))
+         (apply +))))
