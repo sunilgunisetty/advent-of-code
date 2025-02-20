f44b37e309b997d263727eba2f2f1ea41d8a3119
Sunil Gunisetty <sunilgunisetty@gmail.com>
2025-02-20 10:53:03 -0500

    2024: Clojure - day13 part 2

--- clojure/src/clj/aoc/2024/day13.clj
+++ clojure/src/clj/aoc/2024/day13.clj
@@ -14,13 +14,18 @@
 
 (defn parse-equations
   ([equations]
-   (parse-equations equations 1))
+   (parse-equations equations 0))
   ([equations error-adjustment]
    (let [[eq1 eq2 r] (string/split-lines equations)
          [_ a1 a2] (re-find #"Button A: X\+(\d+), Y\+(\d+)" eq1)
          [_ b1 b2] (re-find #"Button B: X\+(\d+), Y\+(\d+)" eq2)
          [_ c1 c2] (re-find #"Prize: X=(\d+), Y=(\d+)" r)]
-     (map parse-long [a1 a2 b1 b2 c1 c2]))))
+     [(parse-long a1)
+      (parse-long a2)
+      (parse-long b1)
+      (parse-long b2)
+      (+ error-adjustment (parse-long c1))
+      (+ error-adjustment (parse-long c2))])))
 
 (defn parse-input
   [input]
@@ -32,6 +37,16 @@
   (->> input
        parse-input
        (map parse-equations)
+       (map solve-equation)
+       (filter (fn [[x y]] (and (int? x) (int? y))))
+       (map (fn [[x y]] (+ (* x 3) y)))
+       (apply +)))
+
+(defn day13-part2
+  [input]
+  (->> input
+       parse-input
+       (map (fn [e] (parse-equations e 10000000000000)))
        (map solve-equation)
        (filter (fn [[x y]] (and (int? x) (int? y))))
        (map (fn [[x y]] (+ (* x 3) y)))
